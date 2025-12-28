a44f54b116af720729c35b62f6a9d7ef8760855b
source-code-examples <coding@eclipso.email>
2025-12-28 15:22:11 -0500

    connect to MongoDB and implement /api/gifts and /api/gifts/:id endpoints

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,34 +1,42 @@
-router.get('/', async (req, res) => {
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+const logger = require('../logger');
+// const { ObjectId } = require('mongodb');
+
+router.get('/', async (req, res, next) => {
+    logger.info('/ called');
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+        const db = await connectToDatabase();
 
         // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        const collection = db.collection("gifts");
 
         // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        // const gifts = {{insert code here}}
+        const gifts = await collection.find({}).toArray();
 
         // Task 4: return the gifts using the res.json method
-        res.json(/* {{insert code here}} */);
+        res.json(gifts);
     } catch (e) {
-        console.error('Error fetching gifts:', e);
+        logger.console.error('Error fetching gifts:', e);
         res.status(500).send('Error fetching gifts');
+        next(e);
     }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
+        const db = await connectToDatabase();
 
         // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        const collection = db.collection("gifts");
 
         const id = req.params.id;
 
         // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-        // {{insert code here}}
+        const gift = await collection.findOne({ id: id });
 
         if (!gift) {
             return res.status(404).send('Gift not found');
@@ -38,6 +46,7 @@ router.get('/:id', async (req, res) => {
     } catch (e) {
         console.error('Error fetching gift:', e);
         res.status(500).send('Error fetching gift');
+        next(e);
     }
 });
 
